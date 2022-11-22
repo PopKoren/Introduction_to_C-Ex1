04135264cea50b08c98f6cfb4180171a7ccbd50c
avihai119 <118832985+avihai119@users.noreply.github.com>
2022-11-22 18:20:17 +0200

    Update #include <stdio.h>.c

--- #include <stdio.h>.c
+++ #include <stdio.h>.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
-
-
-void main()
+int sumDigits(int num)
 {
-adaryh
+	int sum = 0, i;
+
+	while (num > 0)
+	{
+		i = num % 10;
+		sum = sum + i;
+		num = num / 10;
+	}
+
+	return sum;
 }
+
+
+int main()
+{
+	int num, sum;
+
+	printf("Enter a number:");
+	scanf_s("%d", &num);
+
+	sum = sumDigits(num);
+
+	printf("%d", sum);
+}
+
