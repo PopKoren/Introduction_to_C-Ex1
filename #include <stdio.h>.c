e76069b61b18fed6fdef733a77f8e94df0687d92
PopKoren <korenabugov@gmail.com>
2022-11-23 22:18:47 +0200

    Update #include <stdio.h>.c

--- #include <stdio.h>.c
+++ #include <stdio.h>.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <math.h>
+
+
 int sumDigits(int num)
 {
 	int sum = 0, i;
@@ -13,16 +16,46 @@ int sumDigits(int num)
 	return sum;
 }
 
-
-int main()
+int checkID(int id)
 {
-	int num, sum;
+    int sumid = 0, sumid2 = 0, scaleof1;
+    int id2 = id / 10;
 
-	printf("Enter a number:");
-	scanf_s("%d", &num);
+    if (log10(id) + 1 < 10)
+    {
+        while (id > 0)
+        {
+            scaleof1 = id % 10;
+            sumid = sumid + scaleof1;
+            id = id / 100;
+        }
+        
 
-	sum = sumDigits(num);
+        while (id2 > 0) 
+        {
+            scaleof1 = id2 % 10; 
+            scaleof1 = (scaleof1 * 2); 
+            if (scaleof1 >= 10)
+            {
+                scaleof1 = scaleof1 % 10 + 1; 
+                sumid2 = scaleof1 + sumid2; 
+            }
+            else
+                sumid2 = sumid2 + scaleof1;
 
-	printf("%d", sum);
+            id2 = id2 / 100;
+        }
+       
+
+        if ((sumid + sumid2) == 0) return 0;
+        else if ((sumid + sumid2) % 10 == 0)
+        {
+            return 1;
+        }
+        else
+            return 0;
+    }
+    else
+        return 0;
+
 }
-
